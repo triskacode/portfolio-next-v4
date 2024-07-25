a466c57b8586dd51037c7dd721b6f1f711319f23
triska.triskacode <triska.triskacode@gmail.com>
2024-07-25 19:39:26 +0000

    ci: Enhance linting and formatting toolchain
    - Update ESLint configuration with stricter rules
    - Refine lint-staged setup for improved file handling
    - Adjust Prettier configuration for consistent code style
    - Modify Next.js files to comply with updated linting rules
    - Add new package.json scripts for linting and formatting
    - Improve TypeScript and Tailwind configurations

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 const styleguide = require('@vercel/style-guide/prettier');
+
 const { plugins: _plugins } = styleguide;
 
+/** @type {import('prettier').Config} */
 module.exports = {
   ...styleguide,
   plugins: [..._plugins, 'prettier-plugin-tailwindcss'],

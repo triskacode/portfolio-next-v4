66ccaaa10fdac11ef10c79242b1cd360f3c1fb24
Triska Mahfud K <triska.triskacode@gmail.com>
2024-08-24 20:04:09 +0000

    refactor(blog): Update blog page and post card component
    - Replace placeholder data with actual content from '#site/content'
    - Enhance PostCard component with improved accessibility and styling
    - Update Home page to use new PostCard component
    - Adjust layout and styling in blog page for better responsiveness

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,34 +1,36 @@
 import { PostCard } from '@/components/post-card';
-import { posts } from '@/lib/placeholder-data';
+import { posts } from '#site/content';
 
-export default function Blog(): React.JSX.Element {
+export default function Page(): React.JSX.Element {
   return (
     <main className="relative isolate bg-background pb-4 text-foreground md:pb-8 lg:pb-24">
       {/* header intoduction */}
-      <section className="mx-auto grid w-full max-w-2xl grid-cols-1 px-4 py-12 md:max-w-3xl md:px-8 md:py-20 lg:max-w-5xl lg:py-24 xl:max-w-7xl">
-        <div className="flex max-w-3xl flex-col items-stretch justify-center gap-4 md:gap-6 lg:gap-8">
+      <section className="mx-auto grid w-full max-w-2xl grid-cols-1 px-4 py-8 md:max-w-3xl md:px-8 md:py-16 lg:max-w-5xl xl:max-w-7xl">
+        <div className="flex max-w-3xl flex-col items-stretch justify-center gap-4 md:gap-6">
           {/* intro */}
           <div className="flex flex-col items-start justify-center">
             <h1 className="text-balance text-start text-4xl/none font-bold tracking-tight md:text-5xl lg:text-6xl">
-              Fullstack Developer Insights From Code to Case Studies
+              Fullstack Developer Insights.
             </h1>
           </div>
-          {/* brief description */}
+          {/* description */}
           <div className="text-start text-base md:text-lg">
             <p>
-              Explore my journey as a <b>Fullstack Developer</b> through case
-              studies and hands-on experiences. Dive into insights on web
-              technologies and real-world project breakdowns.
+              Explore a Fullstack Developer&apos;s journey through case studies
+              and experiences. Dive into practical solutions and innovative
+              approaches from real-world projects in web development.
             </p>
           </div>
         </div>
       </section>
       <hr />
       {/* blog posts */}
-      <section className="mx-auto grid w-full max-w-2xl grid-cols-1 gap-8 px-4 py-8 md:max-w-3xl md:grid-cols-2 md:p-8 lg:max-w-5xl lg:grid-cols-3 xl:max-w-7xl">
-        {posts.map((post) => (
-          <PostCard key={post.id} post={post} />
-        ))}
+      <section className="mx-auto grid w-full max-w-2xl grid-cols-1 gap-y-10 px-4 py-8 md:max-w-3xl md:grid-cols-2 md:gap-x-8 md:p-8 lg:max-w-5xl lg:grid-cols-3 xl:max-w-7xl">
+        {posts.length > 0 ? (
+          posts.map((post) => <PostCard key={post.slug} post={post} />)
+        ) : (
+          <p>No posts available at the moment.</p>
+        )}
       </section>
     </main>
   );

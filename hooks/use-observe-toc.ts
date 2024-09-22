a85a15eb950c9092b6c1761a567b9408a9ffb0bf
Triska Mahfud K <triska.triskacode@gmail.com>
2024-09-22 16:04:06 +0000

    feat(blog): add table of content to blog posts (#69)
    * feat(blog): add table of content to blog posts
    This commit introduces a table of contents feature for blog posts. It includes the addition of a table of contents to the post page, implementation of a new hook to observe the table of contents, creation of a new component to render the table of contents, and definition of a new type for the Velite table of contents. These enhancements improve navigation and readability for blog readers.
    
    Co-authored-by: Tika <179169711+tikatrsk@users.noreply.github.com>

---
+++ hooks/use-observe-toc.ts
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import { type TocEntry } from '@/types/velite';
+
+function flatItem(item: TocEntry): TocEntry[] {
+  if (!item.items) {
+    return [item];
+  }
+  return [{ ...item, items: [] }, ...item.items.flatMap(flatItem)];
+}
+
+function getTocIds(toc: TocEntry[]): string[] {
+  return toc
+    .flatMap((item) => flatItem(item))
+    .map(({ url }) => url.split('#').slice(1).join('#'));
+}
+
+export function useObserveToc(toc: TocEntry[]): string {
+  const [activeTocId, setActiveTocId] = useState<string>('');
+
+  useEffect(() => {
+    const tocIds = getTocIds(toc);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const activeEntry = entries.find((entry) => entry.isIntersecting);
+        if (activeEntry) {
+          setActiveTocId(activeEntry.target.id);
+        }
+      },
+      {
+        rootMargin: '0px 0px -80% 0px',
+      },
+    );
+
+    tocIds.forEach((id) => {
+      const element = document.getElementById(id);
+      if (element) {
+        observer.observe(element);
+      }
+    });
+
+    return () => {
+      tocIds.forEach((id) => {
+        const element = document.getElementById(id);
+        if (element) {
+          observer.unobserve(element);
+        }
+      });
+      observer.disconnect();
+    };
+  }, [toc]);
+
+  return activeTocId;
+}

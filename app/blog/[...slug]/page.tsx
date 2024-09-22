82160f08d42cf2d7e5ac31869c2157ffeb18f656
Tika <179169711+tikatrsk@users.noreply.github.com>
2024-09-22 16:04:06 +0000

    feat(blog): Add table of content to blog posts
    - Add table of content to post page
    - Add new hook to observe table of content
    - Add new component to render table of content
    - Add new type to define velite table of content

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -3,10 +3,11 @@ import { notFound } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import { posts, type Post } from '#site/content';
-import { formatDate, formatTime } from '@/lib/utils';
+import { cn, formatDate, formatTime } from '@/lib/utils';
 import { MDXContent } from '@/components/mdx-content';
 import { Button } from '@/components/ui/button';
 import { Icon } from '@/components/ui/icon';
+import { PostTableOfContent } from '@/components/toc';
 import { ScrollToTopButton, ShareButton } from './button';
 
 function getPostFromParams(params: { slug?: string[] }): Post | undefined {
@@ -37,59 +38,71 @@ export default function Page({ params }: PageProps): React.JSX.Element {
 
   return (
     <main className="relative isolate bg-background pb-4 text-foreground md:pb-8 lg:pb-24">
-      <section className="mx-auto grid w-full max-w-3xl grid-cols-1 gap-6 px-4 py-6 md:gap-8 md:px-8 md:py-8 lg:py-10">
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <h1 className="text-balance text-start text-4xl/none font-bold tracking-tight md:text-5xl">
-              {post.title}
-            </h1>
+      <section className="mx-auto grid w-full max-w-6xl grid-cols-1 gap-6 px-4 py-6 md:gap-8 md:px-8 md:py-8 lg:grid-cols-[1fr_300px] lg:gap-10 lg:py-10 xl:gap-20">
+        <div className="mx-auto grid max-w-3xl grid-cols-1 gap-6 md:gap-8">
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <h1 className="text-balance text-start text-4xl/none font-bold tracking-tight md:text-5xl">
+                {post.title}
+              </h1>
+            </div>
+            <div className="text-start text-base md:text-lg">
+              <p>{post.description}</p>
+            </div>
+            <div className="flex flex-row flex-wrap gap-2 text-sm">
+              <time
+                dateTime={post.updated ?? post.date}
+                className="text-muted-foreground"
+              >
+                {formatDate(post.updated ?? post.date)}
+                {post.updated ? ' (Updated)' : ''}
+              </time>
+              <span className="text-muted-foreground odd:hidden">·</span>
+              <span className="text-muted-foreground">
+                {formatTime(post.info.readingTime)} read
+              </span>
+              <span className="text-muted-foreground odd:hidden">·</span>
+              <span className="text-muted-foreground">
+                {post.info.wordCount} words
+              </span>
+            </div>
           </div>
-          <div className="text-start text-base md:text-lg">
-            <p>{post.description}</p>
-          </div>
-          <div className="flex flex-row flex-wrap gap-2 text-sm">
-            <time
-              dateTime={post.updated ?? post.date}
-              className="text-muted-foreground"
-            >
-              {formatDate(post.updated ?? post.date)}
-              {post.updated ? ' (Updated)' : ''}
-            </time>
-            <span className="text-muted-foreground odd:hidden">·</span>
-            <span className="text-muted-foreground">
-              {formatTime(post.info.readingTime)} read
-            </span>
-            <span className="text-muted-foreground odd:hidden">·</span>
-            <span className="text-muted-foreground">
-              {post.info.wordCount} words as
-            </span>
+          <Image
+            src={post.cover.src}
+            width={post.cover.width}
+            height={post.cover.height}
+            placeholder="blur"
+            blurDataURL={post.cover.blurDataURL}
+            className="rounded-md border bg-muted transition-colors"
+            alt={`Cover image for ${post.title}`}
+          />
+          <article>
+            <MDXContent code={post.body} />
+          </article>
+          <hr />
+          <div className="flex flex-row items-center justify-between gap-4">
+            <div>
+              <Button variant="outline" asChild>
+                <Link href="/blog">
+                  <Icon.Lucide name="chevron-left" className="mr-2 size-4" />
+                  See all posts
+                </Link>
+              </Button>
+            </div>
+            <div className="flex flex-row items-center justify-end gap-2">
+              <ShareButton post={post} />
+              <ScrollToTopButton />
+            </div>
           </div>
         </div>
-        <Image
-          src={post.cover.src}
-          width={post.cover.width}
-          height={post.cover.height}
-          placeholder="blur"
-          blurDataURL={post.cover.blurDataURL}
-          className="rounded-md border bg-muted transition-colors"
-          alt={`Cover image for ${post.title}`}
-        />
-        <article>
-          <MDXContent code={post.body} />
-        </article>
-        <hr />
-        <div className="flex flex-row items-center justify-between gap-4">
-          <div>
-            <Button variant="outline" asChild>
-              <Link href="/blog">
-                <Icon.Lucide name="chevron-left" className="mr-2 size-4" />
-                See all posts
-              </Link>
-            </Button>
-          </div>
-          <div className="flex flex-row items-center justify-end gap-2">
-            <ShareButton post={post} />
-            <ScrollToTopButton />
+        <div className="hidden text-sm lg:block">
+          <div
+            className={cn(
+              'sticky top-[var(--offset-top)] max-h-[calc(theme(maxHeight.dvh)-var(--offset-top))] overflow-y-auto',
+              '[--offset-top:calc(var(--header-height)+theme(spacing.10))]',
+            )}
+          >
+            <PostTableOfContent toc={post.toc} />
           </div>
         </div>
       </section>
